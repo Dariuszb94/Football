d219fbb3d8929ebfe4212041b898f6a1c381b40e
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-22 18:51:53 +0100

    css font global

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
 import Team from "./components/Team";
 import styled from "styled-components";
 import Header from "./components/Header";
+import "./App.css";
 import { useState } from "react";
 function App() {
   const [reset, resetSet] = useState(false);
@@ -16,8 +16,6 @@ function App() {
 
 export default App;
 const Container = styled.div`
-  @import url("https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,700&display=swap");
-  font-family: "Lato", sans-serif;
   display: flex;
   max-width: 800px;
   margin: 0 auto;
